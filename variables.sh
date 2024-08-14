22af2ae46776435315c174a19cb1550a8c4c45d3
Yashwanth <yrkunduru@joinbia.com>
2024-08-14 14:33:03 +0530

    update
    update

--- variables.sh
+++ variables.sh
@@ -7,19 +7,19 @@ echo -e "Value of x is ${x} and value of y is ${y}"
 
 # system special variables
 
-$0 - name of the script
-$1 - first argument
-$2 - second argument
-$n - nth argument
+# $0 - name of the script
+# $1 - first argument
+# $2 - second argument
+# $n - nth argument
 
-$# - number of arguments
-$* - all arguments
-$? - exit status of last command
-$$ - pid of the script
+# $# - number of arguments
+# $* - all arguments
+# $? - exit status of last command
+# $$ - pid of the script
 
-> - redirect stdout
->> - append stdout
+# > - redirect stdout
+# >> - append stdout
 
-&>/dev/null - redirect stdout and stderr to /dev/null
+# &>/dev/null - redirect stdout and stderr to /dev/null
 
 echo -e $0 $1 $2 $# $* $? $$ > /dev/null
